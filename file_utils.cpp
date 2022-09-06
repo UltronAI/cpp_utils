9c793bfded183b12d259a5930afaa444edb57f3a
fenggao <gaof.student@gmail.com>
2022-09-06 18:47:31 +0800

    add writelines to file_utils

--- file_utils.cpp
+++ file_utils.cpp
@@ -1,6 +1,7 @@
 #include "file_utils.h"
 
 #include <string>
+#include <algorithm>
 
 std::vector<std::string> FUtils::readlines(const std::string file_path) {
 	std::ifstream infile;
@@ -19,4 +20,40 @@ std::vector<std::string> FUtils::readlines(const std::string file_path) {
 
 	infile.close();
 	return lines_buffer;
+}
+
+void FUtils::writelines(std::ofstream& outfile, std::string new_line) {
+	if (!outfile.is_open()) {
+		throw std::exception("file is not open");
+	}
+	outfile << new_line << std::endl;
+}
+
+void FUtils::writelines(std::ofstream& outfile, std::vector<std::string> new_lines) {
+	if (!outfile.is_open()) {
+		throw std::exception("file is not open");
+	}
+	for (std::string line : new_lines) {
+		writelines(outfile, line);
+	}
+}
+
+void FUtils::writelines(std::string file_path, std::string new_line) {
+	std::ofstream outfile;
+	outfile.open(file_path, std::ios::app);
+	if (!outfile.is_open()) {
+		throw std::exception("file is not open");
+	}
+	writelines(outfile, new_line);
+}
+
+void FUtils::writelines(std::string file_path, std::vector<std::string> new_lines) {
+	std::ofstream outfile;
+	outfile.open(file_path, std::ios::app);
+	if (!outfile.is_open()) {
+		throw std::exception("file is not open");
+	}
+	for (std::string line : new_lines) {
+		writelines(outfile, line);
+	}
 }
--- file_utils.h
+++ file_utils.h
@@ -9,6 +9,11 @@ namespace file_utils {
 
 std::vector<std::string> readlines(const std::string file_path);
 
+void writelines(std::ofstream& file_stream, std::string new_line);
+void writelines(std::ofstream& file_stream, std::vector<std::string> new_lines);
+void writelines(std::string file_path, std::string new_line);
+void writelines(std::string file_path, std::vector<std::string> new_lines);
+
 }
 }
 
